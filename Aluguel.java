c6682b10c4fd8bfa462a96d2c50efe45557784b7
Your Name <you@example.com>
2023-09-12 03:20:52 -0300

    Alteração na classe Aluguel, criação da classe Devolução e construçãos dos cases 6 e 7

---
+++ Aluguel.java
@@ -0,0 +1,25 @@
+import java.time.LocalDateTime;
+
+public class Aluguel {
+    private Veiculo veiculo;
+    private Cliente cliente;
+    private LocalDateTime dataHoraInicio;
+
+    public Aluguel(Cliente cliente, Veiculo veiculo, LocalDateTime dataHoraInicio) {
+        this.cliente = cliente;
+        this.veiculo = veiculo;
+        this.dataHoraInicio = dataHoraInicio;
+    }
+
+    public Veiculo getVeiculo() {
+        return veiculo;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public LocalDateTime getDataHoraInicio() {
+        return dataHoraInicio;
+    }
+}
